2a3a4a8af0467d530ffc14ee8b279df3f7d80cde
Mark Hünermund Jensen <mark@hunermund.dk>
2014-03-31 10:38:04 +0200

    Create userdata.php

---
+++ demos/mvc-ajax-app/models/userdata.php
@@ -0,0 +1,14 @@
+<?php
+
+	final class UserData extends Emx\Mvc\Model {
+
+		public function GetBogusUserData(  ) {
+			return array(
+				'Name' 		=> 'John Doe',
+				'Age' 		=> 34
+			);
+		}
+
+	}
+
+?>
